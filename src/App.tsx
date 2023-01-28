2a821c202bf70fd5d452909591da98ffca529d61
jotabraga <jpmbraga.feg@gmail.com>
2023-01-28 17:02:07 -0300

    feat: add chakra ui to design

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import * as React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <h1>Hello luan</h1>
-  )
+    return (
+        <ChakraProvider>
+            <h1>Hello luan</h1>
+        </ChakraProvider>
+    )
 }
 
 export default App
